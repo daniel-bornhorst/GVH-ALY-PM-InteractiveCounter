4139dbd9988105c489f1e98e8b209dcac477db06
Daniel Bornhorst <daniel.bornhorst@meowwolf.com>
2024-10-19 11:41:43 -0500

    All shift in working, teensy ethernet working

--- Firmware/GVH-ALY-PM-InteracticeCounter/GVH-ALY-PM-InteracticeCounter.ino
+++ Firmware/GVH-ALY-PM-InteracticeCounter/GVH-ALY-PM-InteracticeCounter.ino
@@ -8,11 +8,35 @@
 # define DEBUG_PRINT(x)
 #endif
 
+
+#define MY_IP 10,32,16,232
+#define MY_SUBNET 255,255,255,0
+#define UDP_INCOMING_PORT 8888
+#define UDP_REMOTE_PORT 9000
+
+
+//Ethernet
+#include <QNEthernet.h>
+using namespace qindesign::network;
+EthernetUDP Udp;
+
+#include <OSCMessage.h>
+OSCMessage msg;
+
+
 const uint8_t clockPin = 13;
 const uint8_t shiftLoad = 11;
 const uint8_t dataIn = 12;
 
-byte incoming;
+
+elapsedMillis shiftTimer;
+const unsigned int shiftInterval = 100;
+
+
+byte incomingByte1;
+byte incomingByte2;
+byte incomingByte3;
+uint32_t combinedBytes = 0;
 int count = 0;
 
 void setup() {
@@ -26,36 +50,132 @@ void setup() {
   digitalWrite(shiftLoad, HIGH);
 
 
+  //Ethernet
+	uint8_t mac[6];
+	Ethernet.macAddress(mac);  // This is informative; it retrieves, not sets
+  Serial.printf("MAC = %02x:%02x:%02x:%02x:%02x:%02x\r\n",
+		mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+	
+	Ethernet.onLinkState([](bool state) {
+		if (state) {
+			Serial.printf("[Ethernet] Link ON: %d Mbps, %s duplex, %s crossover\r\n",
+				Ethernet.linkSpeed(),
+				Ethernet.linkIsFullDuplex() ? "full" : "half",
+				Ethernet.linkIsCrossover() ? "is" : "not");
+		} 
+    else {
+			Serial.printf("[Ethernet] Link OFF\r\n");
+		}
+	});
+
+
+	Ethernet.begin({MY_IP}, {MY_SUBNET}, {MY_IP});
+	
+	
+	if ( Udp.begin(UDP_INCOMING_PORT) == 1 ) {
+		Serial.print("My IP address: ");
+		Serial.println(Ethernet.localIP());	
+	} 
+  else {
+		Serial.println("No ethernet");
+	}
+	
+	Ethernet.setRetransmissionTimeout(20);
+	Ethernet.setRetransmissionCount(1);
+
+  shiftTimer = 0;
+
 }
 
 void loop() {
 
-  digitalWrite(shiftLoad, LOW);
-  delay(1);
-  digitalWrite(shiftLoad, HIGH);
-  delay(1);
+  if (shiftTimer >= shiftInterval) {
+    digitalWrite(shiftLoad, LOW);
+    delay(1);
+    digitalWrite(shiftLoad, HIGH);
+    delay(1);
 
-  incoming = shiftIn(dataIn, clockPin, LSBFIRST);
-  DEBUG_PRINT("byte ");
-  DEBUG_PRINT(count++);
-  DEBUG_PRINT(": ");
-  printBinary(incoming);
+    DEBUG_PRINT("Touch Sample");
+    //DEBUG_PRINT(count++);
+    DEBUG_PRINT(": ");
 
-  incoming = shiftIn(dataIn, clockPin, LSBFIRST);
-  DEBUG_PRINT(", ");
-  printBinary(incoming);
+    // Byte 3
+    incoming = shiftIn(dataIn, clockPin, MSBFIRST);
+    DEBUG_PRINT(bitRead(incoming, 1)); // We only care about the two least significat bit of the first byte
+    DEBUG_PRINT(bitRead(incoming, 0));
+    //printBinary(incoming);
 
-  incoming = shiftIn(dataIn, clockPin, LSBFIRST);
-  DEBUG_PRINT(", ");
-  printBinary(incoming);
+    DEBUG_PRINT(" ");
 
-  DEBUG_PRINTLN();
-  delay(2000);
+    // Byte 2
+    incoming = shiftIn(dataIn, clockPin, MSBFIRST);
+    printBinary(incoming);
 
+    DEBUG_PRINT(" ");
+
+    // Byte 1
+    incoming = shiftIn(dataIn, clockPin, MSBFIRST);
+    printBinary(incoming);
+
+    DEBUG_PRINTLN();
+    
+    shiftTimer = 0;
+  }
+
+  OSCReceive();
+
+}
+
+uint32_t combineBytes(byte byte1, byte byte2, byte byte3) {
+  uint32_t combinedBytes = 0;
+
+  combinedBytes = (uint32_t)((byte3 << 16) | (byte2 << 8) | (byte1 << 0));
+
+  return combinedBytes;
 }
 
 void printBinary(byte inByte) {
   for (int b = 7; b >= 0; b--) {
     DEBUG_PRINT(bitRead(inByte, b));
   }
+}
+
+void OSCReceive() {
+	int size;
+	
+	static unsigned long microsTime = 0;
+	
+	if( (size = Udp.parsePacket())>0) {
+    microsTime = micros();
+    while(size--) {
+      //Serial.println(size);
+      msg.fill(Udp.read());
+    }
+    if ((micros() - microsTime) > 10000) return; //Timeout for no eoP()
+
+    if(!msg.hasError()) {
+      //need to add all the show manager commands
+      msg.dispatch("/test", testOSC);
+    }	
+    msg.empty();
+  }
+}
+
+
+void testOSC(OSCMessage &msg ) {
+  static int msgCount = 0;
+	Serial.println("__TEST OSC__");	
+  sendOSCMessage(msgCount++);
+}
+
+
+void sendOSCMessage(int argument){
+
+  //The raw dog method for sending OSC
+  OSCMessage msg("/BACK/AT/YA");
+  msg.add(argument);
+  Udp.beginPacket(Udp.remoteIP(), UDP_REMOTE_PORT);
+  msg.send(Udp); // send the bytes to the SLIP stream
+  Udp.endPacket(); // mark the end of the OSC Packet
+  msg.empty(); // free space occupied by message
 }
